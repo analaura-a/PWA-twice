2db1243f0cfb4f60ca7f50eb4fc02b954264cee7
Ana Laura <analaura.almiron@gmail.com>
2022-12-30 21:57:47 -0300

    + index.html

---
+++ serviceWorker.js
@@ -0,0 +1,24 @@
+const staticTwice = "TWICE!";
+const assets = [
+  "./",
+  "./index.html",
+  "./styles/estilos.css",
+  "./script.js"
+
+];
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticTwice).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
